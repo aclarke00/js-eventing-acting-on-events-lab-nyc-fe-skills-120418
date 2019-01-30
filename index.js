68dfeda979d6c3f43c28565a6c2fcf4a2133e513
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-01-30 00:58:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+// EVENT LISTENER
+
+// 1 - find the thing to do the event continue
+// we actually don't need an element (the document will get the event listener)
+
+// 2 - write the function that will happen when we do the thing
+
+const moveDodgerLeft = () => {
+  // get dodger first
+  const dodger = document.getElementById('dodger')
+  // next thing is changing layout / position of dodger, meaning we need css changes
+  
+}
