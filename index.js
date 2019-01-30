a68c037a700f938afae8e6bcbf572bab5ed93ef1
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-01-30 01:38:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ const moveDodgerLeft = () => {
   const currentLeft = dodger.style.left // '180px'
   let currentLeftNumber = currentLeft.replace('px', '') // '180'
   currentLeftNumber = parseInt(currentLeftNumber, 10) // 180
-  dodger.style.left = currentLeftNumber - 15 + 'px' // '179px' 
   if (currentLeftNumber > 0) {
-    
+    dodger.style.left = currentLeftNumber - 15 + 'px' // '179px'
   }
 }
 
